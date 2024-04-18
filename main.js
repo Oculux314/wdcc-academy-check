0006b14ca8118c8fff1807e1f147a6b03989884e
Oculux <nate.williamson.nz@gmail.com>
2024-04-18 15:31:06 +1200

    :sparkles: 3.2 command parsing

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-// alert("main.js online!")
+const programs = new Map();
+
+function load(program) {
+  programs.set(program.name, program);
+}
 
 function execute(event) {
   event.preventDefault();
@@ -6,6 +10,22 @@ function execute(event) {
   const inputELem = document.querySelector("#input");
   const outputElem = document.querySelector("#output");
 
+  const args = commandElem.value.trim().split(" ").filter((arg) => arg !== "");
+  const programName = args[0].toLowerCase();
+  const program = programs.get(programName) ?? (() => "Error: Command not found.");
+
   commandElem.value = "";
-  outputElem.value = inputELem.value;
+  outputElem.value = program(inputELem.value, ...args);
 }
+
+load(function add(input, ...args) {
+  return String(+args[1] + +args[2]);
+});
+
+load(function sub(input, ...args) {
+  return String(+args[1] - +args[2]);
+});
+
+load(function copy(input, ...args) {
+  return input;
+});
